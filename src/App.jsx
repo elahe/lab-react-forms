17b643e643d7e73722dc086192405961896d5675
Elahe <elahe6474@gmai.com>
2026-02-07 21:58:43 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -8,41 +8,86 @@ import studentsData from "./assets/students.json";
 
 function App() {
   const [students, setStudents] = useState(studentsData);
+  const [fullName, setFullname] = useState("")
+  const [image, setImage] = useState("")
+  const [phone, setPhone] = useState("")
+  const [email, setEmail] = useState("")
+  const [program, setProgram] = useState("")
+  const [graduationYear, setGraduationYear] = useState(2023)
+  const [graduated, setGraduated] = useState(false)
 
+  const handleFullName =(event) => {
+    setFullname(event.target.value)
+  }
+  const handleImage =(event) => {
+    setImage(event.target.value)
+  }
+  const handlePhone =(event) => {
+    setPhone(event.target.value)
+  }
+  const handleEmail =(event) => {
+    setEmail(event.target.value)
+  }
+  const handleProgram =(event) => {
+    setProgram(event.target.value)
+  }
+  const handleGraduationYear =(event) => {
+    setGraduationYear(event.target.value)
+  }
+  const handleGraduated =(event) => {
+    setGraduated(event.target.checked)
+  }
+
+  const handleSubmit =(event) => {
+    event.preventDefault();
+    const newStudent = {
+      
+      fullName: fullName,
+      email: email,
+      phone: phone,
+      program: program,
+      image: image,
+      graduationYear: graduationYear,
+      graduated: graduated
+    }
+    let clone = [...students]
+    clone.push(newStudent)
+    setStudents(clone)
+  }
 
   return (
     <div className="App pt-20">
       <Navbar />
 
       {/* FORM */}
-      <form>
+      <form onSubmit={handleSubmit}>
         <span>Add a Student</span>
         <div>
           <label>
             Full Name
-            <input name="fullName" type="text" placeholder="Full Name" />
+            <input name="fullName" type="text" placeholder="Full Name" onChange={handleFullName} value={fullName}/>
           </label>
 
           <label>
             Profile Image
-            <input name="image" type="url" placeholder="Profile Image" />
+            <input name="image" type="url" placeholder="Profile Image" onChange={handleImage} value={image} />
           </label>
 
           <label>
             Phone
-            <input name="phone" type="tel" placeholder="Phone" />
+            <input name="phone" type="tel" placeholder="Phone" onChange={handlePhone} value={phone}/>
           </label>
 
           <label>
             Email
-            <input name="email" type="email" placeholder="Email" />
+            <input name="email" type="email" placeholder="Email" onChange={handleEmail} value={email}/>
           </label>
         </div>
 
         <div>
           <label>
             Program
-            <select name="program">
+            <select name="program" onChange={handleProgram} value={program}>
               <option value="">-- None --</option>
               <option value="Web Dev">Web Dev</option>
               <option value="UXUI">UXUI</option>
@@ -60,12 +105,14 @@ function App() {
               maxLength={4}
               min={2023}
               max={2030}
+              onChange={handleGraduationYear}
+              value={graduationYear}
             />
           </label>
 
           <label>
             Graduated
-            <input name="graduated" type="checkbox" />
+            <input name="graduated" type="checkbox" onChange={handleGraduated} checked={graduated}/>
           </label>
 
           <button type="submit">Add Student</button>
